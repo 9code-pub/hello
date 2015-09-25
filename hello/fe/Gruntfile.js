565b10dbc09266f1184e3055f1e16bfa1d99d703
Halo9Pan <halo9pan@gmail.com>
2015-09-25 11:50:03 +0800

    Grunt example code

--- hello/fe/Gruntfile.js
+++ hello/fe/Gruntfile.js
@@ -2,13 +2,67 @@ module.exports = function(grunt) {
 
   // 配置Grunt各种模块的参数
   grunt.initConfig({
-    jshint: { /* jshint的参数 */ },
-    concat: { /* concat的参数 */ },
-    uglify: { /* uglify的参数 */ },
+    clean: [
+      'dist'
+    ],
+    jshint: {},
+    concat: {
+      options: {
+        separator: ';',
+        sourceMap: true
+      },
+      dist: {
+        src: [
+          'public/components/jquery/dist/jquery.js',
+          'public/components/underscore/underscore.js',
+          'public/components/backbone/backbone.js'
+        ],
+        dest: 'dist/components.min.js'
+      },
+      min: {
+        src: [
+          'public/components/jquery/dist/jquery.min.js',
+          'public/components/underscore/underscore-min.js',
+          'public/components/backbone/backbone-min.js'
+        ],
+        dest: 'dist/components.min.js'
+      }
+    },
+    uglify: {
+      options: {
+        mangle: true,
+        beautify: true
+      },
+      target: {
+        expand: true,
+        cwd: 'boilerplate/js/',
+        src: ['*.js', '!*.min.js'],
+        dest: 'dist/js/',
+        ext: '.min.js'
+      }
+    },
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'boilerplate/css/',
+        src: ['*.css', '!*.min.css'],
+        dest: 'dist/css/',
+        ext: '.min.css'
+      },
+      combine: {
+        files: {
+          'dist/css/public.min.css': [
+            'public/components/bootstrap/dist/css/bootstrap.min.css',
+            'public/components/bootstrap/dist/css/bootstrap-theme.min.css'
+          ]
+        }
+      }
+    },
     watch:  { /* watch的参数 */ }
   });
 
   // 从node_modules目录加载模块文件
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -18,7 +72,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
 
   // 每行registerTask定义一个任务
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify']);
   grunt.registerTask('check', ['jshint']);
+  grunt.registerTask('css', ['cssmin:minify','cssmin:combine']);
 
 };
